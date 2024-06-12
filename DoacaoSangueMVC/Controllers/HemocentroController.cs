5e469477a35daa6f28ed1110f94334eaf3c66441
Nicchorff <pedrokhenrique2003@gmail.com>
2024-06-12 18:49:46 -0300

    a

--- DoacaoSangueMVC/Controllers/HemocentroController.cs
+++ DoacaoSangueMVC/Controllers/HemocentroController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DoacaoSangueMVC.Data;
 using DoacaoSangueMVC.Entities;
+using DoacaoSangueMVC.Models;
+using System.Text;
 
 namespace DoacaoSangueMVC.Controllers
 {
@@ -41,6 +43,58 @@ namespace DoacaoSangueMVC.Controllers
             }
 
             return View(hemocentro);
+        }// GET: Hemocentroe/Details/5
+        public async Task<IActionResult> Agendamento()
+        {
+            return View();
         }
-    }  
+
+        public async Task<IActionResult> Agendar(AgendamentoDTO model)
+        {
+            string url = "https://webhook.app.hubhero.com.br/webhook/48ec13d5-039b-4df0-9c9e-b20c40c76806";
+
+            var data = new
+            {
+                nome = "Érico",
+                idade = 20
+
+            };
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Criar uma instância de HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Enviar a requisição POST
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+
+                    // Ler a resposta da requisição
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Verificar se a requisição foi bem-sucedida
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Requisição POST enviada com sucesso!");
+                        Console.WriteLine("Resposta do servidor: " + responseBody);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro ao enviar a requisição POST.");
+                        Console.WriteLine("Status Code: " + response.StatusCode);
+                        Console.WriteLine("Resposta do servidor: " + responseBody);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ocorreu um erro ao enviar a requisição POST:");
+                    Console.WriteLine(ex.Message);
+                }
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
 }
---
+++ DoacaoSangueMVC/Models/AgendamentoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DoacaoSangueMVC.Models
+{
+    public class AgendamentoDTO
+    {
+        public TimeOnly hora { get; set; }
+        public int data { get; set; }
+        public int idDoador { get; set; }
+    }
+}
